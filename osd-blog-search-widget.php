476626aa8573910c57b2e25303882041e9e2377c
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-04 16:36:36 +0000

    Release 1.2
    git-svn-id: https://plugins.svn.wordpress.org/osd-blog-search-widget/trunk@1038274 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- osd-blog-search-widget.php
+++ osd-blog-search-widget.php
@@ -2,24 +2,25 @@
 	/*
 	Plugin Name: OSD Blog Search Widget
 	Plugin URI: http://outsidesource.com
-	Description: A plugin that adds a new widget that will add a search form for blog posts only.
-	Version: 1.1
+	Description: Adds a widget that adds a search form for searching blog posts only.
+	Version: 1.2
 	Author: OSD Web Development Team
-	Author URI: http://outsidesource.com
+	Text Domain: osd-blog-search-widget
 	License: GPL2v2
 	*/
 	
 	class osd_blog_search_widget extends WP_Widget {
-	 
 		public function __construct() {
 			parent::__construct(
 				'osd_blog_search_widget', // Base ID
-				'OSD Blog Search Widget', // Name
-				array('description' => __('Search form for blog posts only.')) // Args
+				/* translators: Widget Name */
+				__('OSD Blog Search', 'osd-blog-search-widget'), // Name
+				/* translators: Widget Description */
+				array('description' => __('Search form for searching blog posts only.', 'osd-blog-search-widget')) // Args
 			);
 		}
 	 
-	 	//output to the sidebars
+	 	// Output to the sidebars
 		public function widget($args, $instance) {
 			extract($args);	 
 			$title = apply_filters('widget_title', $instance['title']);
@@ -27,54 +28,57 @@
 
 			echo $before_widget;
 	 
-			if(!empty($title)) {
+			if (!empty($title)) {
 				echo $before_title . $title . $after_title;
 			}
 			
-			echo __("<div class='search-form' id='osd-blog-search'>
-						<form method='get' action='".home_url('/')."'>
-							<input type='hidden' name='post_type' value='post' />
-							<input type='text' name='s' id='s' value='' placeholder='Search blog' />
-							<input id='osd-search-submit' class='search-button' type='image' alt='Search' src='".get_bloginfo('template_url')."/images/search.svg' />
-						</form>
-					</div>");
-			
-	 
+			echo "<div class='search-form' id='osd-blog-search'>
+					<form method='get' action='".home_url('/')."'>
+						<input type='hidden' name='post_type' value='post' />
+						<input type='text' name='s' id='s' value='' placeholder='".$instance['placeholder']."' />
+						<input id='osd-search-submit' class='search-button' type='image' alt='Search' src='".get_bloginfo('template_url')."/images/search.svg' />
+					</form>
+				</div>"; 
 			echo $after_widget;
 		}
 	 
-	 	//admin menu options
+	 	// Admin menu options
 		public function form($instance) {
-			$title = isset($instance['title']) ? $instance['title'] : '';
-	 
 			?>
 			<p>
-				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+				<?php /* translators: Widget Field: Title */ ?>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'osd-blog-search-widget'); ?></label> 
+				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance["title"] ); ?>" />
+			</p>
+			<p>
+				<?php /* translators: Widget Field: Placeholder */ ?>
+				<label for="<?php echo $this->get_field_id( 'placeholder' ); ?>"><?php _e('Placeholder:', 'osd-blog-search-widget'); ?></label> 
+				<input class="widefat" id="<?php echo $this->get_field_id( 'placeholder' ); ?>" name="<?php echo $this->get_field_name( 'placeholder' ); ?>" type="text" value="<?php echo esc_attr( $instance["placeholder"] ); ?>" />
 			</p>
             <?php
 		}
 	 
-	 	//load the instance
+	 	// Update the instance
 		public function update($new_instance, $old_instance) {
 			$instance = array();
 			$instance['title'] = strip_tags($new_instance['title']);
-	 
+			$instance['placeholder'] = strip_tags($new_instance['placeholder']);
 			return $instance;
+		}
+
+		// Add functionality to the custom search widget
+		public static function blog_search_filter($query) {
+			if ($query->is_search && !is_admin()) {
+				$post_type = $_GET['post_type'];
+				if (!$post_type) {
+					$post_type = 'any';
+				}
+				$query->set('post_type', $post_type);
+			}
+			return $query;
 		}
 	}
 	add_action('widgets_init', create_function('', 'register_widget("osd_blog_search_widget");'));
-	
-	//add functionality to the custom search widget
-	function osd_blog_search_filter($query) {
-		if($query->is_search && !is_admin()) {
-			$post_type = $_GET['post_type'];
-			if(!$post_type) {
-				$post_type = 'any';
-			}
-			$query->set('post_type', $post_type);
-		}
-		return $query;
-	}
-	add_filter('pre_get_posts','osd_blog_search_filter');
+	add_filter('pre_get_posts', array("osd_blog_search_widget", 'blog_search_filter'));
+	load_plugin_textdomain('osd-blog-search-widget', false, basename(dirname(__FILE__))."/lang/");
 ?>
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: osdwebdev
 Tags: wordpress, blog search, search, blog posts only search, custom search, search only posts
 Requires at least: 3.4
 Tested up to: 4.0
-Stable tag: 1.1
+Stable tag: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -33,6 +33,10 @@ Yes, as users ask us questions.
 2. Search input in left sidebar
 
 == Changelog ==
+
+= 1.2 =
+* Added placeholder field for search input
+* Added internationalization support .pot file
 
 = 1.1 =
 * Removed erroneous PHP notice in debug mode
